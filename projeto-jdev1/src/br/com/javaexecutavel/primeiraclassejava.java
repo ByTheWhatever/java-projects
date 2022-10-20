14dd5c78cccf4863b36c1826cfb7de718008cdfc
Gabriel Feitosa <cabeceirasmax@gmail.com>
2022-10-20 16:08:34 -0300

    Criação do projeto - teste de repositório GIT

---
+++ projeto-jdev1/src/br/com/javaexecutavel/primeiraclassejava.java
@@ -0,0 +1,12 @@
+package br.com.javaexecutavel;
+
+public class primeiraclassejava {
+
+	
+	public static void main(String[] args) {
+		
+	System.out.println("Meu primeiro programa em Java");	
+		
+	}
+
+}
