4a21f8fd6cf969efaefdae64f52843e65fc26893
Gabriel Feitosa <cabeceirasmax@gmail.com>
2022-10-21 14:29:04 -0300

    Cálculo de médias de nota com if, else if e else.

--- projeto-jdev1/src/br/com/javaexecutavel/primeiraclassejava.java
+++ projeto-jdev1/src/br/com/javaexecutavel/primeiraclassejava.java
@@ -2,11 +2,10 @@ package br.com.javaexecutavel;
 
 public class primeiraclassejava {
 
-	
 	public static void main(String[] args) {
-		
-	System.out.println("Meu primeiro programa em Java");	
-		
+
+		System.out.println("Meu primeiro programa em Java");
+
 	}
 
 }
